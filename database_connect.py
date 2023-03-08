210606691b0d78f7a74328083ce17ff33788c7e8
Matthewgallo77 <mbg5733@psu.edu>
2023-03-08 15:33:37 -0500

    working 3-8-23

--- database_connect.py
+++ database_connect.py
@@ -5,25 +5,37 @@ from datetime import datetime
 class DB:    
 
     def __init__(self):
+
+        self.databaseName = 'pilotplant'
+        self.host = 'localhost'
+        self.user = 'adsorptech'
+        self.password = 'root'
+
         self.db = mysql.connector.connect(
-        host="localhost",
-        user="root", # NAME OF USER
-        passwd="notforu;)", # PASSWORD
-        database="test" # DATABASE NAME
+        host=self.host,
+        user=self.user, # NAME OF USER
+        passwd=self.password, # PASSWORD
+        database=self.databaseName # DATABASE NAME
     )
 
     def addValue(self, Value, Name): # UPDATES VALUE AND TIME
         mycursor = self.db.cursor()
-        mycursor.execute("UPDATE TagTable SET Date = %s, Value = %s WHERE Name = %s",(datetime.now(), Value, Name))
+        mycursor.execute(f"UPDATE {self.databaseName} SET Date = %s, Value = %s WHERE Name = %s",(datetime.now(), Value, Name))
         self.db.commit()
  
 
     def formatTable(self, Name, Datatype, Address):
         mycursor = self.db.cursor()
-        mycursor.execute("INSERT IGNORE INTO TagTable (Name, Datatype, Address, Date, Value) VALUES (%s, %s, %s, %s, NULL)", (Name, Datatype, Address, datetime.now()))
+        mycursor.execute(f"INSERT IGNORE INTO {self.databaseName} (Name, Datatype, Address, Date, Value) VALUES (%s, %s, %s, %s, NULL)", (Name, Datatype, Address, datetime.now()))
         self.db.commit()
  
     def createTable(self):
         mycursor = self.db.cursor()
-        mycursor.execute("CREATE TABLE IF NOT EXISTS TagTable (Name VARCHAR(50) PRIMARY KEY, Datatype VARCHAR(10) NOT NULL, Address VARCHAR(10) NOT NULL, Date DATETIME NOT NULL, Value DECIMAL(8,5))")
+        mycursor.execute(f"CREATE TABLE IF NOT EXISTS {self.databaseName} (Name VARCHAR(50) PRIMARY KEY, Datatype VARCHAR(10) NOT NULL, Address VARCHAR(10) NOT NULL, Date DATETIME NOT NULL, Value DECIMAL(8,5))")
         self.db.commit
+
+
+    def createPastTable(self):
+        mycursor = self.db.cursor()
+        query = "CREATE TABLE IF NOT EXISTS TagHistory (Date DATETIME NOT NULL, "
+        
--- main.py
+++ main.py
@@ -10,27 +10,33 @@ import time
 
 
 if __name__ == '__main__':
-    PLC = snap7.client.Client() # CREATE PLC INSTANCE
-    tag_datatype = {'Int':2,'Real':4} # GLOBAL DICT OF DATA TYPE
+    # PLC = snap7.client.Client() # CREATE PLC INSTANCE
+    # tag_datatype = {'Int':2,'Real':4} # GLOBAL DICT OF DATA TYPE
 
-    path = utilities.setPath() # SET PATH OF PLC
-    data_frame = pd.DataFrame(pd.read_excel(path))
-    addresses = data_frame['Logical Address'] 
-    data_types = data_frame['Data Type']
-    names = data_frame['Name']
+    # path = utilities.setPath() # SET PATH OF PLC
+    # data_frame = pd.DataFrame(pd.read_excel(path))
+    # addresses = data_frame['Logical Address'] 
+    # data_types = data_frame['Data Type']
+    # names = data_frame['Name']
 
     # utilities.connectPLC() # CONNECT PLC
     database = DB() # CREATE INSTANCE OF DATABASE
-    DB.createTable(database) # CREATES TABLE TO STORE TAG INFORMATION
-    utilities.formatEntireTable(database, names, data_types, addresses) # FORMATS THE TABLE PROPERLY
-                      
-    while PLC.get_connected():
-        try:
-            for address, data_type, name in zip(addresses, data_types, names): # lOOP THROUGH EACH TAG
-                value = utilities.PLC_READ(address, data_type)
-                time.sleep(1) # STOP FOR ONE SECOND
-                DB.addValue(database, value, name)
+    database.createPastTable()
+    # DB.createTable(database) # CREATES TABLE TO STORE TAG INFORMATION
+    # utilities.formatEntireTable(database, names, data_types, addresses) # FORMATS THE TABLE PROPERLY
 
-        except Snap7Exception as e:
-             print(f"PLC Error: {e}")
+               
+    # while True:
+    #     try:
+    #         if (database.db.is_connected()):
+    #             for address, data_type, name in zip(addresses, data_types, names): # lOOP THROUGH EACH TAG
+    #                 value = utilities.PLC_READ(address, data_type)
+    #                 print(value, name)
+    #                 time.sleep(1) # STOP FOR ONE SECOND
+    #                 DB.addValue(database, value, name)
+    #         else:
+    #             print("Database is not connected")
+
+    #     except Snap7Exception as e:
+    #          print(f"PLC Error: {e}")
                           
